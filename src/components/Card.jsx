ad7ce694f77c0ee284569954f44daa434f6fd286
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-09-11 11:18:35 +1000

    created card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import './card.css'
+
+export default function Card() {
+    return (
+        <div className="card">
+            <div className='image-container'>
+
+            </div>
+            <div className='name-container'> 
+            
+            </div>
+        </div>
+    )
+}
--- src/pages/PlayingPage/Playing.jsx
+++ src/pages/PlayingPage/Playing.jsx
@@ -1,14 +1,17 @@
 import './playing.css'
+import Card from '../../components/Card'
 
 export default function Playing() {
 
     return (
         <main className='playing-main'>
-            <div className='cards'>
-
+            <div className='cards-container'>
+                <Card />
+                <Card />
+                <Card />
             </div>
             <div className='score'>
-
+                <span className='playing-score'>0 / 5</span>
             </div>
         </main>
     )
